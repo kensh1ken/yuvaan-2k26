d3c00078ce76e8cc67a249570b8a49fb814ae3b7
[KenshiKen] <[hallosiddharth@gmail.com]>
2026-03-11 00:24:54 +0530

    v1.0.1: Speed Analytics by Vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -25,6 +26,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <SpeedInsights />
       </body>
     </html>
   );
